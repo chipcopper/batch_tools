21512a8e1fe9683b72dbd53019f97577e67a5311
Chip Copper <chip.copper@broadcom.com>
2022-12-20 13:59:27 -0500

    updated version

--- dryrun.py
+++ dryrun.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Version 22.12.20.1
+# Version 22.12.20.2
 # Copyright 2022 Chip Copper
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this 
